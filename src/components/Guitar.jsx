1b9a79fe645771a97f360c3bda9e20f979ca5c95
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-04 05:00:32 -0600

    PROYECTO Carrito de Compras: Eventos en JSX

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,5 +1,8 @@
 const Guitar = ({ guitar }) => {
-    
+
+    const handleClick= (guitar)=>{
+        console.log(guitar);
+    }
     const {id, name, image, description, price}= guitar
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -18,7 +21,9 @@ const Guitar = ({ guitar }) => {
                     {description}
                 </p>
                 <p className="fw-black text-primary fs-3">${ price }</p>
-                <button type="button" className="btn btn-dark w-100">
+                <button type="button" className="btn btn-dark w-100"
+                onClick={()=>{handleClick(guitar)}}
+                >
                     Agregar al Carrito
                 </button>
             </div>
