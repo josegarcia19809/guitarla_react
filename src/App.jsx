f069322b848b5a6aaf467a59940b531d34aedcac
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-06 21:18:06 -0600

    PROYECTO Carrito de Compras: Detectar si un elemento existe en el carrito

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
     const [cart, setCart] = useState([]);
 
     function addToCart(item) {
+        const itemExists=cart.findIndex((guitar) => guitar.id === item.id);
+        console.log(itemExists)
         setCart(prevCar=>[...prevCar, item])
     }
 
