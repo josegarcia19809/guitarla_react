5ea99b3d8072f4ef40c1e9e6f1ff6d3da467939a
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-06 20:59:35 -0600

    PROYECTO Carrito de Compras: Agregando Guitarras al Carrito

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import {useState} from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
 import {db} from "./data/db";
+import guitar from "./components/Guitar";
 
 function App() {
     const [data, setData] = useState(db);
     const [cart, setCart] = useState([]);
+
+    function addToCart(item) {
+        setCart(prevCar=>[...prevCar, item])
+    }
 
     return (
         <>
@@ -20,6 +25,7 @@ function App() {
                             key={guitar.id}
                             guitar={guitar}
                             setCart={setCart}
+                            addToCart={addToCart}
                         />
                     ))}
                 </div>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,4 +1,4 @@
-const Guitar = ({guitar, setCart}) => {
+const Guitar = ({guitar, addToCart}) => {
 
 
     const {id, name, image, description, price} = guitar
@@ -22,7 +22,7 @@ const Guitar = ({guitar, setCart}) => {
                 <button
                     type="button"
                     className="btn btn-dark w-100"
-                    onClick={() => setCart(prevCart => [...prevCart, guitar])}
+                    onClick={() => addToCart(guitar)}
                 >
                     Agregar al Carrito
                 </button>
