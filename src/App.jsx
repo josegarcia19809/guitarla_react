ee0008b4999e55f623026dd1fe85fc4e90279725
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-10 16:58:58 -0600

    PROYECTO Carrito de Compras: Persistente- Recuperar los productos de LocalStorage y mostrarlos

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import Guitar from "./components/Guitar";
 import {db} from "./data/db";
 
 function App() {
+    const initialCart=()=>{
+        const localStorageCart=localStorage.getItem("cart");
+        return localStorageCart ? JSON.parse(localStorageCart) : [];
+    }
+
     const [data, setData] = useState(db);
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(initialCart);
     const MAX_ITEMS = 5;
     const MIN_ITEMS = 1;
 
